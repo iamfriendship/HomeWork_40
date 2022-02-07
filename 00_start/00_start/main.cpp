e34a71aa841895212d4471d5a360729100f065d1
BuildTools <unconfigured@null.spigotmc.org>
2022-02-07 19:24:12 +0900

    00_git start

---
+++ 00_start/00_start/main.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream> 
+
+#include <crtdbg.h> // 메모리 누수 체크
+
+class Player
+{
+public:
+    int Hp_;
+    int MP_;
+
+public:
+    Player()
+        :Hp_(100), MP_(100)
+    {}
+    ~Player()
+    {}
+};
+
+int main()
+{
+    //leak 체크
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+
+    int heapSize = 100;
+    int* Ptr = new int[100]();
+    delete[] Ptr;
+}
