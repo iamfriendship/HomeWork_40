96e51e690265d077f5cd17474ac9b1036712f3fc
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 20:09:04 +0900

    0222_GameVectorSort 김우정

---
+++ 01_woojeong/00_Homework/0222_GameVector/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include "GameVector.h"
+
+
+int main()
+{
+    {
+        GameVectorSort<int> MyVector = GameVectorSort<int>();
+
+
+        MyVector.push_back(5);
+        MyVector.push_back(10);
+        MyVector.push_back(5);
+        MyVector.push_back(1);
+        MyVector.push_back(2);
+        MyVector.push_back(8);
+        MyVector.push_back(99);
+
+        for (size_t i = 0; i < MyVector.size(); i++)
+        {
+            std::cout << MyVector[i] << std::endl;
+        }
+    }
+
+
+}
